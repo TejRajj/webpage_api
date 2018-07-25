d66c95d672b822e0a23c3888080f27e3462bb8a9
TejRajj <rajtejas18@gmail.com>
2018-07-26 01:01:30 +0530

    again i change

--- webpage_api/urls.py
+++ webpage_api/urls.py
@@ -20,6 +20,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from companies import views
 
 # comment
+#urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
